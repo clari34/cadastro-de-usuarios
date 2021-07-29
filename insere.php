db211fd6b69f17ba25796eecc04a6b5fa5ac67ac
Ana Clara <acsn4@aluno.ifal.edu.br>
2021-07-29 08:08:06 -0300

    Inserindo

--- insere.php
+++ insere.php
@@ -1,27 +1,8 @@
 <?php
-	
-	$username = "ana";
-	$password = "root";
-	$database = "pweb2";
-	$server = "localhost"; 
-	
-	$conexao = new mysqli($server, $username, $password, $database);
-	
-	if($conexao->connect_error){
-		die("Erro na conexão ".$conexao->connect_error);
-	} else {
-		echo "Conexão realizada.";
-		$nomeUsuario = $_REQUEST["usuario"];
-        $nomeUsuario1 = $_REQUEST["usuario1"];
-		$email = $_REQUEST["email"];
-		$senha = $_REQUEST["senha"];
 
-		$sql = "INSERT INTO usuarios (nome_usuario, nome_completo, email, senha) VALUES
-				('$nomeUsuario', '$nomeUsuario1', '$email', '$senha')";
-		if($conexao->query($sql)){
-			echo "Usuário inserido.";
-		} else {
-			echo $conexao->error;
-		}
-	}
+$dsn  = 'mysql:host=localhost;dbname=pweb3';
+$user = 'ana';
+$pass = '';
+
+$pdo = new PDO ($dsn, $user, $pass);
 ?>
