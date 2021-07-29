160ac7a331ae693fe7ba6fa385565a2167835386
Ana Clara <acsn4@aluno.ifal.edu.br>
2021-07-29 09:12:51 -0300

    commit6

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=pweb4';
+$dsn = 'mysql:host=127.0.0.1;dbname=pweb4';
 $user = 'ana';
-$pass = '';
+$pass = 'root';
 
 try {
 
     $pdo = new PDO($dsn, $user, $pass);
-    
+
 } catch (PDOException $ex) {
     echo 'Erro ' .$ex->getMessage();
 }
