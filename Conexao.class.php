35712b41b0cf0c45b7708be4cb8a3ba9b7116577
Ana Clara <acsn4@aluno.ifal.edu.br>
2021-07-29 21:09:42 -0300

    alterando

---
+++ Conexao.class.php
@@ -0,0 +1,26 @@
+<?php
+class Conexao{
+	private $user;
+	private $pass;
+	private $banco;
+	private $servidor;
+	private static $pdo;
+	public function __construct(){		
+		$this->servidor = "localhost";
+		$this->banco = "pweb5";
+		$this->user = "root"; 
+		$this->pass = "";
+	}
+	public function conectar(){
+		try{
+			if(is_null(self::$pdo)){
+				self::$pdo = new PDO("mysql:host=".$this->servidor.";dbname=".$this->banco, $this->user, $this->pass);
+			}
+			return self::$pdo;
+		}catch(PDOException $ex) {
+			echo 'Error: '.$ex->getMessage();
+		}
+	}
+}
+?>
+
